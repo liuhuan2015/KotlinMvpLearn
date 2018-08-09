9ab9eebc3658d18b8d50ed66f0bc207ed46b6df4
liuh <2210712252@qq.com>
2018-08-09 11:53:40 +0800

    添加依赖module：multiple-status-view;BaseActivity,SplashActivity。

---
+++ app/src/main/java/com/liuh/kotlinmvp/ui/activity/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.liuh.kotlinmvp.ui.activity
+
+import com.liuh.kotlinmvp.base.BaseActivity
+
+/**
+ * Date: 2018/8/9 11:49
+ * Description:
+ */
+class SplashActivity : BaseActivity() {
+
+    override fun getLayoutId(): Int {
+        return 0
+    }
+
+    override fun initData() {
+
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun start() {
+
+    }
+}
